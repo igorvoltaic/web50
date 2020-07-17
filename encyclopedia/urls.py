6c89d3a3eaadfefe55eae4255fd965ccf9a2152a
Igor Voltaic <ibolsch@gmail.com>
2020-07-17 13:42:43 +0300

    Done: query into the search box in the sidebar to search for an encyclopedia entry

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("create", views.create, name="create")
+    path("create", views.create, name="create"),
+    path("search", views.search, name="search")
 ]
