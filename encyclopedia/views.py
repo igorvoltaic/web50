75b78ff410c69894d66a63ee3b2da1d0efc95b23
Igor Voltaic <ibolsch@gmail.com>
2020-07-17 11:41:14 +0300

    Done (including sub requerements): Clicking “Create New Page” in the sidebar should take the user to a page where they can create a new encyclopedia entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,24 @@
-from django.http import Http404
 from django.views.defaults import page_not_found
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+
 from markdown2 import markdown
 
 from . import util
+
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Entry Title")
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'style': 'height:400px;'}),label="Entry Content")
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.lower() in [x.lower() for x in util.list_entries()]:
+            raise forms.ValidationError("You already have such entry!")
+        return title
+
 
 
 def index(request):
@@ -14,8 +29,25 @@ def index(request):
 
 def entry(request, title):
     if not util.get_entry(title):
-        return page_not_found(request, True, template_name='404.html')
+        return page_not_found(request, True, template_name="404.html")
     return render(request, "encyclopedia/entry.html", {
         "title": title,
         "entry": markdown(util.get_entry(title))
+    })
+
+
+def create(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            util.save_entry(title, content)
+            return HttpResponseRedirect(reverse('entry', args=[title]))
+        else:
+            return render(request, "encyclopedia/create.html", {
+                "form": form
+            })
+    return render(request, "encyclopedia/create.html", {
+        "form": NewEntryForm()
     })
