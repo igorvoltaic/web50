6ac57aad00886e248fd834d061005c4cdf34d2f6
Igor Voltaic <ibolsch@gmail.com>
2020-08-04 17:56:51 +0300

    implemented watchlist

--- auctions/views.py
+++ auctions/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 
@@ -9,7 +10,8 @@ from .models import User, Listing, Bid, Comment, Category, Watchlist
 
 def index(request):
     return render(request, "auctions/index.html", {
-        "listings": Listing.objects.all()
+        "listings": Listing.objects.all(),
+        "heading": "Active Listings"
     })
 
 
@@ -20,20 +22,41 @@ def categories(request):
 
 def category(request, category_id):
     return render(request, "auctions/index.html", {
-        "listings": Listing.objects.filter(category_id=cat_id)
+        "listings": Listing.objects.filter(category_id=category_id),
+        "heading": "Category: " + Category.objects.get(pk=category_id).name
     })
 
 
+@login_required
 def watchlist(request):
-    return HttpResponse('OK')
+    return render(request, "auctions/index.html", {
+        "listings": [l.listing for l in
+            Watchlist.objects.filter(user_id=get_user(request).id)],
+        "heading": "Watched Listings"
+    })
 
 
+@login_required
+def add_watchlist(request, listing_id):
+    # Check the watchlist, add/remove item and return current state to JS
+    watched = Watchlist.objects.filter(user_id=get_user(request).id, listing_id=listing_id)
+    if watched:
+        Watchlist.objects.get(pk=watched[0].id).delete()
+        return JsonResponse(True, safe=False)
+    else:
+        watch = Watchlist(listing_id=listing_id,
+                user_id=get_user(request).id)
+        watch.save()
+        return JsonResponse(False, safe=False)
+
+
+@login_required
 def create_listing(request):
     return HttpResponse('OK')
 
 
 def listing(request, listing_id):
-    return render(request, "auctions/index.html", {
+    return render(request, "auctions/listing.html", {
         "listing": Listing.objects.get(pk=listing_id)
     })
 
