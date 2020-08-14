7299ae16a41e277064dc60c607a086fd23b21fdf
Igor Voltaic <ibolsch@gmail.com>
2020-08-14 13:44:16 +0300

    implemented archive

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
     document.querySelector('#compose').addEventListener('click', compose_email);
     document.querySelector('#compose-form').onsubmit = sendmail;
+    // document.querySelectorAll('#archive').forEach(button => {button.onclick = archive_email});
 
     // By default, load the inbox
     load_mailbox('inbox');
@@ -59,18 +60,25 @@ function load_mailbox(mailbox) {
     // history.pushState({mailbox: mailbox}, "", `${mailbox}`);
 }
 
-function show_mails(name) {
-    fetch(`/emails/${name}`)
+function show_mails(mailbox) {
+    fetch(`/emails/${mailbox}`)
     .then(response => response.json())
     .then(emails => {
 
         //Create and append element for each email
         emails.forEach(emails => {
             const element = document.createElement('div');
-            // element.className = 'emails read';
-            element.className = 'shadow p-3 mb-1 read rounded border';
-            element.innerHTML = emails['sender'] + '  :  ' + emails['subject'] + '  :  ' + emails['timestamp'];
-            element.addEventListener('click', function() {
+            element.className = 'shadow p-2 mb-1 read rounded border container';
+            if (mailbox === 'inbox' || mailbox === 'archive') {
+                if (emails['archived']) {
+                    element.innerHTML = `<div class="row"><div class="col-10">${emails['sender']}  ::  ${emails['subject']}  ::  ${emails['timestamp']}</div><button class="ml-5 btn btn-sm btn-outline-primary" id="archive">Unarchive</button></div>`;
+                } else {
+                    element.innerHTML = `<div class="row"><div class="col-10">${emails['sender']}  ::  ${emails['subject']}  ::  ${emails['timestamp']}</div><button class="ml-5 btn btn-sm btn-outline-primary" id="archive">Archive</button></div>`;
+                }
+            } else {
+                element.innerHTML = `<div class="row"><div class="col-10">${emails['sender']}  ::  ${emails['subject']}  ::  ${emails['timestamp']}</div></div>`;
+            }
+            element.children[0].children[0].addEventListener('click', function() {
                 if (!emails['read']) {
                     fetch(`/emails/${emails['id']}`, {
                         method: 'PUT',
@@ -80,7 +88,7 @@ function show_mails(name) {
                     })
 
                     // Mark clicked email as read
-                    element.className = 'shadow p-3 mb-1 read border rounded';
+                    element.className = 'shadow p-2 mb-1 read border rounded';
                 }
                 fetch(`/emails/${emails['id']}`)
                 .then(response => response.json())
@@ -91,7 +99,41 @@ function show_mails(name) {
                 });
             });
             if (!emails['read']) {
-                element.className = 'shadow p-3 mb-1 rounded unread';
+                element.className = 'shadow p-2 mb-1 rounded unread';
+            }
+            if (mailbox === 'inbox' || mailbox === 'archive') {
+                element.children[0].children[1].addEventListener('click', function() {
+                    event.preventDefault()
+                    if (!emails['archived']) {
+                        fetch(`/emails/${emails['id']}`, {
+                            method: 'PUT',
+                            body: JSON.stringify({
+                                archived: true
+                            })
+                        })
+                        .then(result => {
+                            element.style.animationPlayState = 'running';
+                            element.addEventListener('animationend', () => {
+                                element.remove();
+                                load_mailbox('inbox');
+                            })
+                        });
+                    } else {
+                        fetch(`/emails/${emails['id']}`, {
+                            method: 'PUT',
+                            body: JSON.stringify({
+                                archived: false
+                            })
+                        })
+                        .then(result => {
+                            element.style.animationPlayState = 'running';
+                            element.addEventListener('animationend', () => {
+                                element.remove();
+                                load_mailbox('inbox');
+                            })
+                        });
+                    }
+                });
             }
             document.querySelector('#emails-view').append(element);
         });
