adc8c8b8cf2be6a25a9a2ffde91903b5628918d4
Igor Voltaic <ibolsch@gmail.com>
2020-08-26 08:56:58 +0300

    changed css

--- network/static/network/Navigation.js
+++ network/static/network/Navigation.js
@@ -1,7 +1,19 @@
- var navbar = new Vue({
-      delimiters: ['[[', ']]'],
-      el: '#navbar',
-      data: {
-          navLinks: navLinks
-      }
- });
+Vue.config.ignoredElements = [/^ion-/]
+
+var navbar = new Vue({
+    delimiters: ['[[', ']]'],
+    el: '#navbar',
+    data: {
+        navLinks: navLinks,
+        menuActive: false
+    },
+    methods: {
+        slice: function(s) {
+            return(s.slice(0,1))
+        },
+        setMenuActive: function() {
+            // this.menuActive = !this.menuActive;
+            console.log(this.menuActive)
+        }
+    }
+});
